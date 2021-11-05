4efc110a686f30c9b8849e163483227b5660e668
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2021-11-04 21:41:03 -0300

    Desenvolvimento frot parte 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState }  from 'react';
+import Notes from './Components/Notes';
+import api from './services/api';
+import './CSS/App.css'
+import './CSS/global.css';
+import './CSS/sidebar.css';
+import './CSS/main.css';
+
+
 
 function App() {
+  const [ title, setTitle ] = useState('');
+  const [ notes, setNotes ] = useState('');
+  const [ allNotes, setAllNotes ] = useState([]);
+  
+  useEffect(() => {
+    async function getAllNotes() {
+      const response = await api.get('/annotations');
+
+      setAllNotes(response.data)
+    }
+
+    getAllNotes();
+  }, [])
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+
+    const response =  await api.post('/annotations', {
+      title,
+      notes
+    });
+
+    setTitle('');
+    setNotes('');
+    setAllNotes([...allNotes, response.data]);
+
+    console.log('aqui', response)
+
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Caderno de Tarefas</strong>
+        <form onSubmit={ handleSubmit }>
+          <div className="input-block">
+            <label htmlFor="title">Título da anotação</label>
+            <input
+              required
+              value={title}
+              onChange={ e => setTitle(e.target.value) }
+            />
+          </div>
+          <div className="input-block">
+            <label htmlFor="nota">Anotações</label>
+            <textarea className="textareatest"
+              required
+              value={notes}
+              onChange={ e => setNotes(e.target.value) }
+            />
+            <button type="submit" className="btn1">Salvar</button>
+          </div>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          { allNotes.map(data => (
+            <Notes data={ data } />
+          )) }
+        </ul>
+      </main>
     </div>
   );
 }
